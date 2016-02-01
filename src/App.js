9b0557a740145f1cc345a0242e1d5bebda8ae7cf
Ahmed Abbas <ahmed1490@gmail.com>
2016-02-02 00:12:06 +0100

    stripped down to minimum structure

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react-native';
+const {
+  View,
+  StyleSheet,
+  Text
+} = React;
+
+const Map = React.createClass({
+
+  getInitialState() {
+    return {};
+  },
+
+  render() {
+    return (<Text>Hello world</Text>);
+  },
+
+});
+
+const styles = StyleSheet.create({
+});
+
+export default Map;
