dc47efe83fa357e3411015b7a1e5bb869c6312f6
Ahmed Abbas <ahmed1490@gmail.com>
2016-02-03 01:18:35 +0100

    Zoom into the map on gettin users location

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ const {
   StyleSheet,
   Text
 } = React;
-import GeoInfo from './components/GeoInfo'
+import GeoInfo from './components/GeoInfo';
+import MapBlock from './components/MapBlock';
 
 const App = React.createClass({
 
@@ -13,12 +14,25 @@ const App = React.createClass({
   },
 
   render() {
-    return (<GeoInfo />);
+    return (
+      <View style={styles.container}>
+        <MapBlock />
+      </View>
+    );
   },
 
 });
 
 const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
 });
 
 export default App;
